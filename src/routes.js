8a750219b92b21e278ce19aceebb3348262eb8e4
Michel Câmara <michelvictor16@gmail.com>
2019-10-31 16:35:41 -0400

    Atualizacao de repositorio
    Havia uma pasta que nao deveria ter subido.
    Realocado a pasta img.

--- src/routes.js
+++
@@ -1,9 +0,0 @@
-import {createStackNavigator} from 'react-navigation';
-
-import Main from './pages/main';
-
-export default createStackNavigator({
-  // eslint-disable-next-line prettier/prettier
-  Main,
-
-});
